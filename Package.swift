10ba335896b0aefa3017e71ada4e5eddb7826adc
PhuocTran <tranhuuphuoc88@gmail.com>
2021-01-18 21:12:54 +0700

    set swift tool-version 5.3

---
+++ Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "FSPagerView",
+    platforms: [.iOS(.v8)],
+    products: [
+        .library(name: "FSPagerView", targets: ["FSPagerView"]),
+    ],
+    targets: [
+        .target(name: "FSPagerView", path: "Sources", exclude: ["FSPagerViewObjcCompat.h", "FSPagerViewObjcCompat.m"]),
+    ],
+    swiftLanguageVersions: [.v5]
+)
